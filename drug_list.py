982242fc6c99ef3e5b7db2cafafd8eac08c41187
Henry Mitchell <henry@henrymitchell.org>
2023-01-25 17:00:31 -0500

    Added antibiotics to drug lists

--- drug_list.py
+++ drug_list.py
@@ -31,6 +31,53 @@ def read_list(path: str) -> pd.DataFrame:
     return data
 
 
+def read_antibiotic_list(path: str) -> pd.DataFrame:
+    columns = [
+        "Drug name", "Target", "Class", "MOA", "Unique toxicity", "Unique PK"
+    ]
+    raw = pd.read_excel(path)
+    # Removing empty rows
+    raw = raw.dropna(how="all")
+    raw.columns = raw.columns.str.strip()
+    # Renaming the empty column to "Drug name"
+    raw = raw.rename(columns={"Unnamed: 2": "Drug name"})
+    # ffilling the "Target" column
+    raw["Target"] = raw["Target"].ffill()
+    raw = raw.fillna("None")
+    for column in columns:
+        raw[column] = raw[column].str.strip().str.capitalize()
+    # Stacking ["MSSA", "MRSA", "E. coli", "Pseudomonas", "B. fragilis",
+    # "Mycoplasma"] into one column
+    data = raw.set_index(columns).stack()
+    data = data.str.strip()
+    # Removing the "No" values
+    data = data[data != "No"]
+    # Making the non-standard values explicit
+    data = " (" + data.str.capitalize() + ")"
+    data = data.rename("Addenda")
+    data = data.reset_index()
+    data = data.rename(columns={"level_6": "Indication"})
+    data.loc[data["Addenda"] == " (Yes)", "Addenda"] = ""
+    data["Indication"] += data["Addenda"]
+    data = data.drop(columns="Addenda")
+    # Combining the indications into one comma-separated column
+    data = data.groupby(columns).agg(", ".join).reset_index()
+
+    # Now, we account for rows in the original dataset that have no indication
+    if len(raw) > len(data):
+        # We get the rows that are in the original dataset but not in the
+        # processed one
+        missing = raw[~raw["Drug name"].isin(data["Drug name"])]
+        # We add them to the processed dataset
+        data = pd.concat([data, missing])
+        # And we fill the "Indication" column with "None"
+        data = data.fillna("None")
+
+    data = data[columns + ["Indication"]]
+
+    return data
+
+
 def make_all_info_card() -> tuple[str, str]:
     front = f"{{{{{FIELDS[0]}}}}}\n<br>"
     for field in FIELDS[1:]:
@@ -72,6 +119,10 @@ def main():
                         help="Path to the output file. Defaults to stdout.",
                         nargs=1,
                         default=None)
+    parser.add_argument("-a",
+                        "--antibiotics",
+                        action="store_true",
+                        help="Whether the drug list is an antibiotic list.")
     parser.add_argument("drug_list",
                         help="Path(s) to the drug list(s).",
                         nargs="+")
@@ -81,12 +132,17 @@ def main():
     else:
         output = args.output[0]
 
+    if args.antibiotics:
+        func = read_antibiotic_list
+    else:
+        func = read_list
+
     # Reading each drug list
     data = pd.DataFrame()
     for path in args.drug_list:
         if not os.path.isfile(path):
             raise FileNotFoundError(f"File {path} not found.")
-        data = pd.concat([data, read_list(path)])
+        data = pd.concat([data, func(path)])
 
     # Outputting
     data.to_csv(output, index=False, sep=";", header=False)
